b67ef547ddc5cec36fc1b338e64afee40193a58c
Rodrigo Luger <rodluger@gmail.com>
2021-05-11 11:28:18 -0400

    version bump

--- vspace/__init__.py
+++ vspace/__init__.py
@@ -6,10 +6,9 @@ vspace package init file.
 
 """
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = "1.0"
+__version__ = "1.0.1"
 __author__ = "Caitlyn Wilhelm (cwilhelm@uw.edu)"
 __copyright__ = "Copyright 2020  Caitlyn Wilhelm"
 
